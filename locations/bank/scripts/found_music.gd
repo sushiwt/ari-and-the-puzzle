b9bb150d1cb6dc374905a0a7f7f27ab374644aa4
sushiwt <yoshianimateapples@gmail.com>
2025-11-25 20:31:30 +1000

    var music_playing: bool = false

--- locations/bank/scripts/found_music.gd
+++ locations/bank/scripts/found_music.gd
@@ -1,9 +1,12 @@
 extends AudioStreamPlayer
 
-func _ready() -> void:
-	play()
-	
+var music_playing: bool = false
+
 func _process(_delta:float) -> void:
+	if $"../Camera".position.y <= 200 && music_playing == false:
+		play()
+		music_playing = true
+	
 	if GameState.player_contacted:
 		volume_db = 0
 	else:
--- locations/bank/scripts/music.gd
+++ locations/bank/scripts/music.gd
@@ -2,10 +2,13 @@ extends AudioStreamPlayer
 
 var found_sound = load("res://music/found.mp3")
 
-func _ready() -> void:
-	play()
+var music_playing: bool = false
 
 func _process(_delta:float) -> void:
+	if $"../Camera".position.y <= 200 && music_playing == false:
+		play()
+		music_playing = true
+	
 	#pitch_scale = 1 - (GameState.player_seen * 0.05)
 	if GameState.player_contacted:
 		volume_db = -1000
